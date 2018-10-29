c8a102449b48f29fd1ff61147c1a8b941b47e96f
Robert Lin <robertlin1@gmail.com>
2018-10-28 19:33:21 -0700

    middleware: add auth interceptors tests

--- middleware/auth.go
+++ middleware/auth.go
@@ -11,21 +11,20 @@ import (
 // NewServerInterceptors creates unary and stream interceptors that validate
 // requests, for use with gRPC servers, using given key
 func NewServerInterceptors(key string) (
-	unaryInterceptor func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error),
-	streamInterceptor func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error,
+	unaryInterceptor UnaryInterceptor,
+	streamInterceptor StreamInterceptor,
 ) {
 	unaryInterceptor = func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		meta, ok := metadata.FromIncomingContext(ctx)
 		if !ok {
 			return nil, grpc.Errorf(codes.Unauthenticated, "missing context metadata")
 		}
-		if len(meta["token"]) != 1 {
-			return nil, grpc.Errorf(codes.Unauthenticated, "invalid token")
+		if err := validate(meta, key); err != nil {
+			return nil, err
 		}
-		if meta["token"][0] != key {
-			return nil, grpc.Errorf(codes.Unauthenticated, "invalid token")
+		if handler == nil {
+			return nil, nil
 		}
-
 		return handler(ctx, req)
 	}
 
@@ -34,16 +33,25 @@ func NewServerInterceptors(key string) (
 		if !ok {
 			return grpc.Errorf(codes.Unauthenticated, "missing context metadata")
 		}
-		if len(meta["token"]) != 1 {
-			return grpc.Errorf(codes.Unauthenticated, "invalid token")
+		if err := validate(meta, key); err != nil {
+			return err
 		}
-		if meta["token"][0] != key {
-			return grpc.Errorf(codes.Unauthenticated, "invalid token")
+		if handler == nil {
+			return nil
 		}
-
-		err := handler(srv, stream)
-		return err
+		return handler(srv, stream)
 	}
 
 	return
+}
+
+func validate(meta metadata.MD, key string) error {
+	keys, ok := meta["key"]
+	if !ok || len(meta["key"]) == 0 {
+		return grpc.Errorf(codes.Unauthenticated, "no key provided")
+	}
+	if keys[0] != key {
+		return grpc.Errorf(codes.Unauthenticated, "invalid key")
+	}
+	return nil
 }
