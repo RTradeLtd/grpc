ca40414e731ca636139a6bf6b87c41e117e5edac
potsables <postables@rtradetechnologies.com>
2018-11-25 21:17:06 -0800

    krab: update proto definitions

--- krab/service.pb.go
+++ krab/service.pb.go
@@ -5,11 +5,10 @@ package krab
 
 import (
 	fmt "fmt"
-	math "math"
-
 	proto "github.com/golang/protobuf/proto"
 	context "golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
+	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -54,39 +53,78 @@ func (m *Empty) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Empty proto.InternalMessageInfo
 
-type Key struct {
+type KeyGet struct {
+	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *KeyGet) Reset()         { *m = KeyGet{} }
+func (m *KeyGet) String() string { return proto.CompactTextString(m) }
+func (*KeyGet) ProtoMessage()    {}
+func (*KeyGet) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a0b84a42fa06f626, []int{1}
+}
+
+func (m *KeyGet) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_KeyGet.Unmarshal(m, b)
+}
+func (m *KeyGet) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_KeyGet.Marshal(b, m, deterministic)
+}
+func (m *KeyGet) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_KeyGet.Merge(m, src)
+}
+func (m *KeyGet) XXX_Size() int {
+	return xxx_messageInfo_KeyGet.Size(m)
+}
+func (m *KeyGet) XXX_DiscardUnknown() {
+	xxx_messageInfo_KeyGet.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_KeyGet proto.InternalMessageInfo
+
+func (m *KeyGet) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+type KeyPut struct {
 	PrivateKey           []byte   `protobuf:"bytes,1,opt,name=privateKey,proto3" json:"privateKey,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *Key) Reset()         { *m = Key{} }
-func (m *Key) String() string { return proto.CompactTextString(m) }
-func (*Key) ProtoMessage()    {}
-func (*Key) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a0b84a42fa06f626, []int{1}
+func (m *KeyPut) Reset()         { *m = KeyPut{} }
+func (m *KeyPut) String() string { return proto.CompactTextString(m) }
+func (*KeyPut) ProtoMessage()    {}
+func (*KeyPut) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a0b84a42fa06f626, []int{2}
 }
 
-func (m *Key) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Key.Unmarshal(m, b)
+func (m *KeyPut) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_KeyPut.Unmarshal(m, b)
 }
-func (m *Key) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Key.Marshal(b, m, deterministic)
+func (m *KeyPut) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_KeyPut.Marshal(b, m, deterministic)
 }
-func (m *Key) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Key.Merge(m, src)
+func (m *KeyPut) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_KeyPut.Merge(m, src)
 }
-func (m *Key) XXX_Size() int {
-	return xxx_messageInfo_Key.Size(m)
+func (m *KeyPut) XXX_Size() int {
+	return xxx_messageInfo_KeyPut.Size(m)
 }
-func (m *Key) XXX_DiscardUnknown() {
-	xxx_messageInfo_Key.DiscardUnknown(m)
+func (m *KeyPut) XXX_DiscardUnknown() {
+	xxx_messageInfo_KeyPut.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Key proto.InternalMessageInfo
+var xxx_messageInfo_KeyPut proto.InternalMessageInfo
 
-func (m *Key) GetPrivateKey() []byte {
+func (m *KeyPut) GetPrivateKey() []byte {
 	if m != nil {
 		return m.PrivateKey
 	}
@@ -104,7 +142,7 @@ func (m *Response) Reset()         { *m = Response{} }
 func (m *Response) String() string { return proto.CompactTextString(m) }
 func (*Response) ProtoMessage()    {}
 func (*Response) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a0b84a42fa06f626, []int{2}
+	return fileDescriptor_a0b84a42fa06f626, []int{3}
 }
 
 func (m *Response) XXX_Unmarshal(b []byte) error {
@@ -134,24 +172,27 @@ func (m *Response) GetStatus() string {
 
 func init() {
 	proto.RegisterType((*Empty)(nil), "krab.Empty")
-	proto.RegisterType((*Key)(nil), "krab.Key")
+	proto.RegisterType((*KeyGet)(nil), "krab.KeyGet")
+	proto.RegisterType((*KeyPut)(nil), "krab.KeyPut")
 	proto.RegisterType((*Response)(nil), "krab.Response")
 }
 
 func init() { proto.RegisterFile("service.proto", fileDescriptor_a0b84a42fa06f626) }
 
 var fileDescriptor_a0b84a42fa06f626 = []byte{
-	// 158 bytes of a gzipped FileDescriptorProto
+	// 183 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x2d, 0x4e, 0x2d, 0x2a,
 	0xcb, 0x4c, 0x4e, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0xc9, 0x2e, 0x4a, 0x4c, 0x52,
-	0x62, 0xe7, 0x62, 0x75, 0xcd, 0x2d, 0x28, 0xa9, 0x54, 0x52, 0xe5, 0x62, 0xf6, 0x4e, 0xad, 0x14,
-	0x92, 0xe3, 0xe2, 0x2a, 0x28, 0xca, 0x2c, 0x4b, 0x2c, 0x49, 0xf5, 0x4e, 0xad, 0x94, 0x60, 0x54,
-	0x60, 0xd4, 0xe0, 0x09, 0x42, 0x12, 0x51, 0x52, 0xe2, 0xe2, 0x08, 0x4a, 0x2d, 0x2e, 0xc8, 0xcf,
-	0x2b, 0x4e, 0x15, 0x12, 0xe3, 0x62, 0x2b, 0x2e, 0x49, 0x2c, 0x29, 0x2d, 0x06, 0xab, 0xe3, 0x0c,
-	0x82, 0xf2, 0x8c, 0x52, 0xb9, 0xd8, 0x83, 0x21, 0x56, 0x09, 0xe9, 0x70, 0xf1, 0xba, 0xa7, 0x96,
-	0x04, 0xc0, 0xf5, 0x0b, 0x71, 0xea, 0x81, 0xac, 0xd5, 0xf3, 0x4e, 0xad, 0x94, 0xe2, 0x83, 0x30,
-	0x61, 0xc6, 0x29, 0x31, 0x80, 0x54, 0x07, 0x94, 0x12, 0xab, 0x3a, 0x89, 0x0d, 0xec, 0x0f, 0x63,
-	0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0x88, 0xa2, 0x02, 0x0b, 0xd8, 0x00, 0x00, 0x00,
+	0x62, 0xe7, 0x62, 0x75, 0xcd, 0x2d, 0x28, 0xa9, 0x54, 0x92, 0xe1, 0x62, 0xf3, 0x4e, 0xad, 0x74,
+	0x4f, 0x2d, 0x11, 0x12, 0xe2, 0x62, 0xc9, 0x4b, 0xcc, 0x4d, 0x95, 0x60, 0x54, 0x60, 0xd4, 0xe0,
+	0x0c, 0x02, 0xb3, 0x95, 0x34, 0xc0, 0xb2, 0x01, 0xa5, 0x25, 0x42, 0x72, 0x5c, 0x5c, 0x05, 0x45,
+	0x99, 0x65, 0x89, 0x25, 0xa9, 0xde, 0xa9, 0x95, 0x60, 0x35, 0x3c, 0x41, 0x48, 0x22, 0x4a, 0x4a,
+	0x5c, 0x1c, 0x41, 0xa9, 0xc5, 0x05, 0xf9, 0x79, 0xc5, 0xa9, 0x42, 0x62, 0x5c, 0x6c, 0xc5, 0x25,
+	0x89, 0x25, 0xa5, 0xc5, 0x50, 0xb3, 0xa0, 0x3c, 0xa3, 0x6c, 0x2e, 0xf6, 0x60, 0x88, 0x5b, 0x84,
+	0xf4, 0xb9, 0x78, 0xdd, 0x53, 0x4b, 0x02, 0xe0, 0xfa, 0x85, 0x78, 0xf4, 0x40, 0xee, 0xd2, 0x83,
+	0xb8, 0x45, 0x8a, 0x0f, 0xc2, 0x83, 0x99, 0xa8, 0xc4, 0x00, 0xd2, 0x10, 0x50, 0x8a, 0x5d, 0x43,
+	0x40, 0x29, 0x16, 0x0d, 0x49, 0x6c, 0x60, 0xef, 0x1a, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0x68,
+	0x0e, 0x50, 0x28, 0xff, 0x00, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -166,8 +207,8 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ServiceClient interface {
-	GetPrivateKey(ctx context.Context, in *Key, opts ...grpc.CallOption) (*Response, error)
-	PutPrivateKey(ctx context.Context, in *Key, opts ...grpc.CallOption) (*Response, error)
+	GetPrivateKey(ctx context.Context, in *KeyGet, opts ...grpc.CallOption) (*Response, error)
+	PutPrivateKey(ctx context.Context, in *KeyPut, opts ...grpc.CallOption) (*Response, error)
 }
 
 type serviceClient struct {
@@ -178,7 +219,7 @@ func NewServiceClient(cc *grpc.ClientConn) ServiceClient {
 	return &serviceClient{cc}
 }
 
-func (c *serviceClient) GetPrivateKey(ctx context.Context, in *Key, opts ...grpc.CallOption) (*Response, error) {
+func (c *serviceClient) GetPrivateKey(ctx context.Context, in *KeyGet, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
 	err := c.cc.Invoke(ctx, "/krab.Service/GetPrivateKey", in, out, opts...)
 	if err != nil {
@@ -187,7 +228,7 @@ func (c *serviceClient) GetPrivateKey(ctx context.Context, in *Key, opts ...grpc
 	return out, nil
 }
 
-func (c *serviceClient) PutPrivateKey(ctx context.Context, in *Key, opts ...grpc.CallOption) (*Response, error) {
+func (c *serviceClient) PutPrivateKey(ctx context.Context, in *KeyPut, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
 	err := c.cc.Invoke(ctx, "/krab.Service/PutPrivateKey", in, out, opts...)
 	if err != nil {
@@ -198,8 +239,8 @@ func (c *serviceClient) PutPrivateKey(ctx context.Context, in *Key, opts ...grpc
 
 // ServiceServer is the server API for Service service.
 type ServiceServer interface {
-	GetPrivateKey(context.Context, *Key) (*Response, error)
-	PutPrivateKey(context.Context, *Key) (*Response, error)
+	GetPrivateKey(context.Context, *KeyGet) (*Response, error)
+	PutPrivateKey(context.Context, *KeyPut) (*Response, error)
 }
 
 func RegisterServiceServer(s *grpc.Server, srv ServiceServer) {
@@ -207,7 +248,7 @@ func RegisterServiceServer(s *grpc.Server, srv ServiceServer) {
 }
 
 func _Service_GetPrivateKey_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Key)
+	in := new(KeyGet)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -219,13 +260,13 @@ func _Service_GetPrivateKey_Handler(srv interface{}, ctx context.Context, dec fu
 		FullMethod: "/krab.Service/GetPrivateKey",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ServiceServer).GetPrivateKey(ctx, req.(*Key))
+		return srv.(ServiceServer).GetPrivateKey(ctx, req.(*KeyGet))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
 func _Service_PutPrivateKey_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Key)
+	in := new(KeyPut)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -237,7 +278,7 @@ func _Service_PutPrivateKey_Handler(srv interface{}, ctx context.Context, dec fu
 		FullMethod: "/krab.Service/PutPrivateKey",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ServiceServer).PutPrivateKey(ctx, req.(*Key))
+		return srv.(ServiceServer).PutPrivateKey(ctx, req.(*KeyPut))
 	}
 	return interceptor(ctx, in, info, handler)
 }
