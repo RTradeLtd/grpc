5089a55696771b6f5f50c0a7b21b3f12d6c2a81a
Alex Trottier <postables@rtradetechnologies.com>
2020-01-03 17:11:17 -0800

    dialer: enable key control for dialer

--- dialer/credentials.go
+++ dialer/credentials.go
@@ -3,25 +3,24 @@ package dialer
 import (
 	"context"
 
-	"github.com/RTradeLtd/grpc/middleware"
 	"google.golang.org/grpc/credentials"
 )
 
 // Credentials holds per-rpc metadata for the gRPC clients
 type Credentials struct {
-	token  string
-	secure bool
+	key, value string
+	secure     bool
 }
 
-// NewCredentials instantiates a new credentials container
-func NewCredentials(token string, withTransportSecurity bool) credentials.PerRPCCredentials {
-	return Credentials{token, withTransportSecurity}
+// NewCredentials instantiates a new credentials container that uses an arbitrary key/value combination.
+func NewCredentials(key, value string, withTransportSecurity bool) credentials.PerRPCCredentials {
+	return Credentials{key, value, withTransportSecurity}
 }
 
 // GetRequestMetadata retrieves relevant metadata
 func (c Credentials) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
 	return map[string]string{
-		middleware.AuthorizationKey: c.token,
+		c.key: c.value,
 	}, nil
 }
 
